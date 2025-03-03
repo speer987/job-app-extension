6c0aa8ec43ffbb5073406aeedfaf5f57412ebc5c
speer987 <saarah.peer15@gmail.com>
2025-03-03 15:28:13 -0800

    adding updated extension that works

---
+++ side_panel.js
@@ -0,0 +1,48 @@
+console.log("Side panel working");
+import config from "./config";
+
+const SPREADSHEET_ID = config.SPREADSHEET_ID;
+const API_KEY = config.API_KEY;
+const RANGE = "A:A";
+
+async function fetchData() {
+  console.log("yes");
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${RANGE}?key=${API_KEY}`;
+  const today = new Date();
+  const today_date = `${
+    today.getMonth() + 1
+  }/${today.getDate()}/${today.getFullYear()}`;
+  let date_count = 0;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.values) {
+      data.values.map((row) => {
+        const spreadsheet_date = row[0].split(" ")[0];
+        console.log(spreadsheet_date);
+        console.log(today_date);
+        if (spreadsheet_date === today_date) {
+          date_count += 1;
+        }
+      });
+      const data_len = data.values.length - 1;
+      document.getElementById("dateCount").textContent = date_count;
+      document.getElementById("dataLen").textContent = data_len;
+
+      const progressBarDaily = document.getElementById("progressBarDaily");
+      progressBarDaily.value = date_count;
+
+      const progressBarTotal = document.getElementById("progressBarTotal");
+      progressBarTotal.value = data_len;
+    } else {
+      console.error("No data found.");
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+setInterval(fetchData, 5000);
+
+fetchData();
