5e93700c0fc6832e8af6ebb1c11e2270314d1476
speer987 <saarah.peer15@gmail.com>
2025-03-03 20:51:51 -0800

    updating polling intervals

--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 });
+
+// Maybe implement a way to only call the api when the side panel is open.
+// chrome.action.onClicked.addListener(() => {
+//   chrome.sidePanel.getPanelState((state) => {
+//     if (state === "opened") {
+//       console.log("The side panel is open.");
+//     } else {
+//       console.log("The side panel is closed.");
+//     }
+//   });
+// });
